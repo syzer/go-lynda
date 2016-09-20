b0ccd2f2e50505e0b932b11a8cf2d94ccd117167
syzer3@gmail.com <syzer3@gmail.com>
2016-09-20 10:18:40 +0200

    read file

--- read-file/main.go
+++ read-file/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -15,13 +16,29 @@ func main() {
 
 	rdr := csv.NewReader(f)
 	rdr.Comma = '\t'
+	rdr.TrimLeadingSpace = true
 
 	rows, err := rdr.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 
-	for _, row := range rows {
-		fmt.Println(row)
+	// atmospheric, baromertic, wind speed
+	var atTotal, bmTotal, wsTotal, count float64
+
+	for i, row := range rows {
+		if i != 0 {
+			at, _ := strconv.ParseFloat(row[1], 64)
+			bm, _ := strconv.ParseFloat(row[2], 64)
+			ws, _ := strconv.ParseFloat(row[7], 64)
+			atTotal += at
+			bmTotal += bm
+			wsTotal += ws
+			count++
+		}
 	}
+	fmt.Println("Total ", count)
+	fmt.Println("Mean Air ", atTotal / count)
+	fmt.Println("Mean Barometric ", bmTotal / count)
+	fmt.Println("Mean  Wind Speed ", wsTotal / count)
 }
