e167648fa8b89fa88990e68534f96082fdc3b59a
syzer3@gmail.com <syzer3@gmail.com>
2016-09-21 09:19:42 +0200

    parsing remote file

--- http_get/main.go
+++ http_get/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"net/http"
+	"github.com/syzer/go-lynda/lib"
 	"log"
-	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	res, err := http.Get("http://stackoverflow.com/questions/24780213/d3-js-how-to-rotate-the-texts-on-each-bar-of-a-grouped-bar-chart")
+	res, err := http.Get("http://localhost:8080/Environmental_Data_Deep_Moor_2012.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	page, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+	rdr := csv.NewReader(res.Body)
+	rdr.Comma = '\t'
+	rdr.TrimLeadingSpace = true
+
+	defer res.Body.Close()
+
+	rows, err := rdr.ReadAll()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s", page)
+	fmt.Println("Total ", len(rows)-1)
+	fmt.Println("Mean Air ", lib.Mean(rows, 1), " Median ", lib.Median(rows, 1))
+	fmt.Println("Mean Barometric ", lib.Mean(rows, 2), " Median ", lib.Median(rows, 2))
+	fmt.Println("Mean Wind Speed ", lib.Mean(rows, 7), " Median ", lib.Median(rows, 7))
 }
